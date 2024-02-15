72a9d0b308da3f0deb505f094e75f1c1d2226f14
Albert Andrews <03jayteee@gmail.com>
2024-02-15 12:14:44 +0000

    Navbar component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Navbar from "./Components/Navbar";
+
 function App() {
   return (
-    <div>
-     
+    <div className="bg-[#FFFAFA] h-full">
+      <Navbar />
     </div>
   );
 }
---
+++ src/Components/Navbar.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { DiGhostSmall } from "react-icons/di";
+
+const Navbar = () => {
+  return (
+    <div className="flex flex-wrap items-center justify-between mx-auto p-4">
+      <h1>Beats</h1>
+      <div className="flex">
+        <DiGhostSmall size={20} className="md:hidden" />
+        <ul className="hidden md:flex gap-2 w-full md:w-auto">
+          <li>Home</li>
+          <li>Specs</li>
+          <li>Case</li>
+          <li>Poduct</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
