274366562e40fe00fceb63bb02caf63a467744d4
Albert Andrews <03jayteee@gmail.com>
2024-02-23 09:56:03 +0000

    update

--- src/Pages/Button.js
+++ src/Pages/Button.js
@@ -7,7 +7,7 @@ const Button = () => {
   const toggling = () => setIsOpen(!isOpen);
 
   return (
-    <div className="xl:hidden relative flex justify-center items-center ">
+    <div className="xl:hidden  relative flex justify-center items-center">
       <div>
         <button onClick={toggling}>
           <RiMenu4Line size={30} />
@@ -15,8 +15,8 @@ const Button = () => {
       </div>
 
       {isOpen && (
-        <div className="absolute   bg-white dark:bg-[#323536] border shadow-md right-0 left-0 px-8 py-7">
-          <ul className="flex flex-col justify-center items-center text-black  mt-[4rem]  w-full">
+        <div className="absolute   dark:bg-[#323536] border shadow-md right-10 top-10 p-9 bg-orange-300">
+          <ul className="flex flex-col justify-center items-center text-black  mt-[4rem] bg-white ">
             <li
               key="home"
               className="hover:text-bold hover:text-white scale-110"
@@ -39,9 +39,8 @@ const Button = () => {
               key="product"
               className="hover:text-bold hover:text-white scale-110"
             >
-             Product
+              Product
             </li>
-           
           </ul>
         </div>
       )}
