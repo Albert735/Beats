235b3ec7772c95efff45d6bc3e49d7b0794227bf
Albert Andrews <03jayteee@gmail.com>
2024-02-26 20:53:31 +0000

    update

--- src/Router.js
+++
@@ -1,9 +0,0 @@
-import { createBrowserRouter } from "react-router-dom";
-import Case from "./Pages/Case";
-
-export const router = createBrowserRouter([
-    {
-   path:"#",
-   element:<Case/>
-    },
-]) 
