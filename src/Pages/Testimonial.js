4ee86c345684dac930fad33c1b5d098502dee66e
Albert Andrews <03jayteee@gmail.com>
2024-03-05 15:05:04 +0000

    Testimonial text clip update

--- src/Pages/Testimonial.js
+++ src/Pages/Testimonial.js
@@ -2,22 +2,28 @@ import React from "react";
 import man from "../assets/images/man.jpeg";
 
 const Testimonial = () => {
+  const germanFlag = "🇩🇪";
+  const unitedStates = "	🇺🇸";
   return (
     <div className="flex  relative  flex-col justify-center  items-center max-w-screen-xl mx-auto my-[4rem]">
       <div>
         <h1 className="text-[2rem] ml-[3rem] md:ml-0 md:text-[4rem]  font-mono font-extralight leading-tight xl:leading-snug md:leading-tight ">
-          Our members' <br /> insight and takeaways
+          Our members' <br /> <span className="bg-clip-text text-transparent bg-gradient-to-b from-[#847B7C] to-[#A59F9F] ">insight </span>  and <span className="bg-clip-text text-transparent bg-gradient-to-b from-[#847B7C] to-[#A59F9F] ">takeaways</span>
         </h1>
       </div>
       <div className="flexflex-row-reverse xl:pb-[35rem]  ">
         <div className="absolute right-[15rem] xl:top-[13rem]">
-          <img src={man} alt="" className="xl:h-[30rem]  rounded-[1rem]   " />
+          <img
+            src={man}
+            alt=""
+            className="xl:h-[30rem]  rounded-[1rem] brightness-125   "
+          />
         </div>
 
         <div className="space-y-5 absolute xl:right-[30rem] xl:top-[23rem]  ">
-          <div className=" p-4 bg-transparent backdrop-blur-xl rounded-[1rem] space-y-3">
+          <div className=" px-7 py-5  bg-black/10 hover:text-gray-200 dark:bg-white/5 hover:bg-black/30 dark:hover:bg-gradient-to-r from-white/30 to-white/10 hover:delay-150  backdrop-blur-xl transition-all hover:ease-in-out hover:duration-200 rounded-[1rem] space-y-3">
             <div>
-              <span className="text-[2.5rem] font-serif italic">"</span>
+              <span className="text-[2rem] font-serif italic">"</span>
             </div>
 
             <div>
@@ -32,9 +38,15 @@ const Testimonial = () => {
               </p>
             </div>
 
-            <div>
-              <p className="font-sans text-[1.3rem] text-slate-700 dark:text-slate-500">
-                Christian Hamilton
+            <div className="gap-2">
+              <p className="font-sans text-[1rem] text-slate-700 dark:text-slate-400">
+                Music Producer
+              </p>
+              <p className="font-sans text-[1.3rem] text-slate-700 dark:text-slate-400 ">
+                Christian Hamilton{" "}
+                <span>
+                  {germanFlag}/{unitedStates}
+                </span>
               </p>
             </div>
           </div>
