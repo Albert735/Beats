ad2ac74cff2c93f1f3613997f9466f031b83da24
Albert Andrews <03jayteee@gmail.com>
2024-03-05 17:34:59 +0000

    Testimonial mobile view positioning

--- src/Pages/Testimonial.js
+++ src/Pages/Testimonial.js
@@ -5,9 +5,9 @@ const Testimonial = () => {
   const germanFlag = "🇩🇪";
   const unitedStates = "🇺🇸";
   return (
-    <div className="flex xl:relative flex-col md:flex-col justify-center  items-center max-w-screen-xl mx-auto md:my-[4rem] my-20">
-      <div className="flex justify-center items-start m-8">
-        <h1 className="text-[2rem] ml-[3rem] md:ml-0 md:text-[4rem]  font-mono font-extralight leading-tight xl:leading-normal md:leading-tight ">
+    <div className="flex relative flex-col md:flex-col justify-center  items-center max-w-screen-xl mx-auto md:my-[4rem] my-20">
+      <div className="flex justify-center items-start ">
+        <h1 className="text-[2.5rem] ml-[3rem] md:ml-0 md:text-[4rem]  font-mono font-extralight leading-tight xl:leading-normal md:leading-tight ">
           Our members' <br />{" "}
           <span className="bg-clip-text text-transparent bg-gradient-to-b from-[#847B7C] to-[#A59F9F] ">
             insight{" "}
@@ -18,28 +18,28 @@ const Testimonial = () => {
           </span>
         </h1>
       </div>
-      <div className="xl:flex-row-reverse  xl:mb-[35rem] flex justify-center items-center flex-col   ">
+      <div className="xl:flex-row-reverse  xl:mb-[35rem] flex justify-center items-center flex-col    ">
         <div className="xl:absolute right-[15rem] xl:top-[13rem] items-center justify-center flex">
           <img
             src={man}
             alt=""
-            className="xl:h-[30rem] md:h-[10rem]   md:rounded-[1rem] brightness-125   "
+            className="xl:h-[30rem] md:h-[10rem] w-[23rem] rounded-[1rem] md:rounded-[1rem] brightness-125"
           />
         </div>
 
-        <div className="xl:space-y-5 xl:absolute xl:right-[30rem] xl:top-[23rem]   flex justify-center items-center ">
-          <div className=" px-7 py-5  bg-black/10 hover:text-gray-200 dark:bg-white/5 hover:bg-black/30 dark:hover:bg-gradient-to-r from-white/20 to-white/10 hover:delay-150  backdrop-blur-xl transition-all hover:ease-in-out hover:duration-200 rounded-[1rem] space-y-3">
+        <div className="xl:space-y-5  absolute xl:right-[30rem]  xl:top-[23rem]   top-[27rem] flex justify-center items-center ">
+          <div className="px-7 py-5  bg-black/10 hover:text-gray-200 dark:bg-white/5 hover:bg-black/30 dark:hover:bg-gradient-to-r from-white/20 to-white/10 hover:delay-150  backdrop-blur-xl transition-all hover:ease-in-out hover:duration-200 rounded-[1rem] md:rounded-[1rem] space-y-3">
             <div>
               <span className="text-[2rem] font-serif italic">"</span>
             </div>
 
             <div>
-              <p className="font-sans md:text-[1.3rem] ">
+              <div className="font-sans md:text-[1.3rem] md:w-[37rem] xl:w-[35rem] w-[20rem]">
                 I've learned a positively Immeasurable amount of things from{" "}
                 Timbaland and Natalie Portman. It gave me sense of possibility.{" "}
                 Like I can actually do it.I can get a billboard hit single, and{" "}
                 it's not far away."
-              </p>
+              </div>
             </div>
 
             <div className="gap-2">
