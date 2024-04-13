2bda4349e0a1fafde6fb3b0f706f436eb38a7f10
Albert Andrews <03jayteee@gmail.com>
2024-04-13 12:58:12 +0000

    csrt modal update

--- src/Sections/CartModal.js
+++ src/Sections/CartModal.js
@@ -5,26 +5,24 @@ import { TiShoppingCart } from "react-icons/ti";
 import CartItems from "../CartItems";
 import { useStateContext } from "../Context/StateContext";
 import { Link } from "react-router-dom";
+import { IoIosInformationCircle } from "react-icons/io";
 
 const CartModal = () => {
   const [isOpen, setIsOpen] = useState(false);
   const toggling = () => setIsOpen(!isOpen);
 
-
   const toggleDropdown = () => {
     if (isOpen !== null) {
       setIsOpen(!isOpen);
     } else {
-      console.error('toggleDropdown encountered null isOpen value');
+      console.error("toggleDropdown encountered null isOpen value");
     }
-
   };
-
 
   const { cartItems, totalQuantity, totalPrice } = useStateContext();
 
   return (
-    <div className=" relative  ">
+    <div>
       <span className="relative flex h-5 w-5">
         <span className="absolute inline-flex rounded-full h-4 w-4 bg-[#DF1D3C] left-3 -top-1 text-white text-[10px] justify-center items-center">
           {totalQuantity}
@@ -40,7 +38,7 @@ const CartModal = () => {
           data-aos="fade-left"
           data-aos-duration="1000"
           data-aos-delay="ease-in-out"
-          className="flex flex-col fixed justify-end items-end top-0 right-0 w-screen backdrop-blur-md bg-opacity-5  h-screen "
+          className="flex flex-col fixed justify-end items-end  top-0 right-0 w-[100vw] backdrop-blur-md bg-opacity-5  h-screen z-50"
         >
           <div className="xl:w-[30rem] relaive w-full h-screen overflow-y-scroll scroll-mb-[90rem] bg-white">
             <div className="flex relative inset-0 z-20 justify-between items-center p-2 text-black md:p-9">
@@ -80,27 +78,39 @@ const CartModal = () => {
               <div></div>
             ) : (
               <div className="flex  flex-col justify-between items-center xl:w-36">
-                <div className="flex flex-col justify-start items-center text-black text-[1rem]  w-screen md:w-[38rem] xl:w-[28rem] p-5 py-1 gap-2   ">
+                <div className="flex flex-col justify-start items-center text-black text-[1rem]  w-screen md:w-[38rem] xl:w-[28rem] p-5  space-y-2  ">
+                  <p className="flex justify-between items-center w-full  rounded-xl ">
+                    <span className="font-light">Subtotal</span>
+                    <span className="font-light">Ghc {totalPrice}</span>
+                  </p>
+                  <p className="flex justify-between items-center w-full  rounded-xl font-bold">
+                    <span className="font-light">Discount</span>
+                    <span className="font-light">Ghc 20%</span>
+                  </p>
+                  <p className="flex justify-between items-center w-full rounded-xl font-bold">
+                    <span className="flex justify-center items-center gap-1 font-light" >
+                      Tax
+                      <IoIosInformationCircle />
+                    </span>
+                    <span className="font-light">Ghc </span>
+                  </p>
+                </div>
+                <div className="flex justify-between items-center text-black text-[1rem]  w-screen md:w-[38rem] xl:w-[28rem] px-5">
+                  <span>Total</span>
+                  <span className="font-bold">Ghc {totalPrice}</span>
+                </div>
+                <div className="flex flex-col justify-start items-center text-black text-[1rem]  w-screen md:w-[38rem] xl:w-[28rem] p-5 gap-3">
                   <button
                     type="button"
-                    className="flex justify-between items-center w-full bg-gray-100 p-5 rounded-xl "
+                    className="flex justify-center items-center border border-gray-300  px-5 py-4 rounded-xl w-full  font-bold"
                   >
-                    <span>Total</span>
-                    <span>Ghc {totalPrice}</span>
-                  </button>
+                    <Link to="/">Continue Shopping</Link>
+                  </button>{" "}
                   <button
                     type="button"
-                    className="flex justify-between items-center w-full bg-gray-100 p-5 rounded-xl font-bold"
+                    className="flex justify-center items-center bg-[#000000] px-5 py-4 rounded-xl w-full text-white font-bold"
                   >
-                    <span>Tax</span>
-                    <span>Ghc 20%</span>
-                  </button>
-
-                  <button
-                    type="button"
-                    className="flex justify-center items-center bg-[#000000] p-5 rounded-xl w-full text-white font-bold"
-                  >
-                    <Link to='/checkout'>Checkout </Link>
+                    <Link to="/checkout">Checkout </Link>
                   </button>
                 </div>
               </div>
